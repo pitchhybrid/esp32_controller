9eb0922cf54f69050b84b1b0cb1c917dbf6d9c56
PitchHybrid <messias_junior@outlook.com>
2022-10-06 01:05:17 -0300

    mais

--- lib/tone/tone.cpp
+++ lib/tone/tone.cpp
@@ -1,5 +1,12 @@
 #include "tone.hpp"
 #include <Arduino.h>
+
+struct Melody {
+    int freq;
+    int duration;
+};
+
+
 void Tone::iniciar(){}
 
 void Tone::_tone(int frequency,long duration){
@@ -12,4 +19,13 @@ void Tone::_tone(int frequency){
 
 void Tone::_noTone(){
     noTone(pin);
+}
+
+void Tone::_tone(Melody melody){
+    _tone(melody.freq,melody.duration);
+}
+
+template<typename... Args>
+void Tone::melody(Melody melody ...){
+    int dummy[] = { 0, ( (void) _tone(std::forward<Args>(melody)), 0) ... };
 }
--- lib/tone/tone.hpp
+++ lib/tone/tone.hpp
@@ -1,11 +1,17 @@
 #include "component.hpp"
 
+struct Melody;
+
 class Tone: public Component {
 
     public:
     Tone(int _pin): Component(_pin){}
     void iniciar();
+    void _tone(Melody melody);
     void _tone(int frequency, long duration);
     void _tone(int frequency);
     void _noTone();
+    template<typename... Args>
+    void melody(Melody melody ... );
 };
+
